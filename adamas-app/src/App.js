f8e06ba99f1bbf4228e6130f524a73c5f97e39c1
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-07-19 19:22:16 +0200

    add basic react-qr-scanner

--- adamas-app/src/App.js
+++ adamas-app/src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import React, { useState } from "react";
+import QrReader from "react-qr-scanner";
 
 function App() {
+  const [test, setTest] = useState("");
+
+  const handleScan = (result) => {
+    if (result) {
+      setTest(result);
+      console.log(result);
+    }
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
   return (
     <div className="App">
       <h1>Hello Adamas</h1>
+      <h2>Scanned: {test.text}</h2>
+      <QrReader delay={500} onError={handleError} onScan={handleScan} />
     </div>
   );
 }
