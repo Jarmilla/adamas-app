3582a49523dfbb7969e9a09f577191ea4e2162bf
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-07-27 21:10:14 +0200

    mapping through data names in CollectionOfData

--- adamas-app/src/components/CollectionOfData.js
+++ adamas-app/src/components/CollectionOfData.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-function CollectionOfData() {
+function CollectionOfData({ data }) {
   return (
-    <div>
+    <div className="collection-of-data">
       <h1>CollectionOfData</h1>
+      {data &&
+        Object.keys(data.items).map((key) => {
+          let value = data.items[key];
+          console.log(key, value);
+          return <h2 key={key}>{value.name}</h2>;
+        })}
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 }
