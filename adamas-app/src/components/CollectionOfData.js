4741ea85eb1013de4078957a6888ed4d7e99ab7c
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-07-28 12:00:18 +0200

    PieceOfData.js button hiding/showing information

--- adamas-app/src/components/CollectionOfData.js
+++ adamas-app/src/components/CollectionOfData.js
@@ -4,13 +4,12 @@ import PieceOfData from "./PieceOfData";
 function CollectionOfData({ data }) {
   return (
     <div className="collection-of-data">
-      <h1>CollectionOfData</h1>
+      <h1>Összegyűjtött Adatok</h1>
       {data &&
         Object.keys(data.items).map((key) => {
           let value = data.items[key];
           return <PieceOfData key={value.name} pieceOfData={value} />;
         })}
-      <p>{JSON.stringify(data)}</p>
     </div>
   );
 }
--- adamas-app/src/components/PieceOfData.js
+++ adamas-app/src/components/PieceOfData.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PieceOfData({ pieceOfData }) {
+  const [isClicked, setIsCliked] = useState(false);
+  const [isDataShown, setDataShown] = useState(false);
+
   return (
     <div className="piece-of-data">
-      <h2> {pieceOfData.name}</h2>
-      <p>{pieceOfData.type}</p>
-      <ul>
-        {pieceOfData.description.map((trait) => (
-          <li>{trait}</li>
-        ))}
-      </ul>
-      {Object.keys(pieceOfData.coordinates).map((value) => (
-        <p>
-          {value} : {pieceOfData.coordinates[value]}
-        </p>
-      ))}
+      <h2
+        className={` button-effect ${isClicked ? "selected" : null}`}
+        onClick={() => {
+          setDataShown(!isDataShown);
+          setIsCliked(!isClicked);
+        }}
+      >
+        {pieceOfData.name}
+      </h2>
+
+      {isDataShown && (
+        <div>
+          <h3>Olvasás részletes adatai:</h3>
+          <p>Típus: {pieceOfData.type}</p>
+          <ul>
+            {pieceOfData.description.map((trait, key) => (
+              <li key={key}>{trait}</li>
+            ))}
+          </ul>
+
+          <h3>Bemérve itt: </h3>
+          {Object.keys(pieceOfData.coordinates).map((value) => (
+            <p key={value}>
+              {value} : {pieceOfData.coordinates[value]}
+            </p>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
