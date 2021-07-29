eebf8e4f8b680a0feb9b9da2ab2c3ca424324b41
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-07-29 09:23:35 +0200

    CollectionOfData shows list or PieceOfData

--- adamas-app/src/components/PieceOfData.js
+++ adamas-app/src/components/PieceOfData.js
@@ -1,39 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 
 function PieceOfData({ pieceOfData }) {
-  const [isClicked, setIsCliked] = useState(false);
-  const [isDataShown, setDataShown] = useState(false);
-
   return (
     <div className="piece-of-data">
-      <h2
-        className={` button-effect ${isClicked ? "selected" : null}`}
-        onClick={() => {
-          setDataShown(!isDataShown);
-          setIsCliked(!isClicked);
-        }}
-      >
-        {pieceOfData.name}
-      </h2>
+      <h2 className="button-effect selected">{pieceOfData.name}</h2>
 
-      {isDataShown && (
-        <div>
-          <h3>Olvasás részletes adatai:</h3>
-          <ul>
-            <li>Típus: {pieceOfData.type}</li>
-            {pieceOfData.description.map((trait, key) => (
-              <li key={key}>{trait}</li>
-            ))}
-          </ul>
+      <h3>Letapogatás eredménye:</h3>
+      <ul>
+        <li>Típus: {pieceOfData.type}</li>
+        {pieceOfData.description.map((trait, key) => (
+          <li key={key}>{trait}</li>
+        ))}
+      </ul>
 
-          <h3>Bemérve itt: </h3>
-          {Object.keys(pieceOfData.coordinates).map((value) => (
-            <p key={value}>
-              {value} : {pieceOfData.coordinates[value]}
-            </p>
-          ))}
-        </div>
-      )}
+      <h3>Bemérve itt: </h3>
+      {Object.keys(pieceOfData.coordinates).map((value) => (
+        <p key={value}>
+          {value} : {pieceOfData.coordinates[value]}
+        </p>
+      ))}
     </div>
   );
 }
