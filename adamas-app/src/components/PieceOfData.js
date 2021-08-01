793e91cf18861aecb4778eab7683b4810e286cf0
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-08-01 14:08:09 +0200

    add geo distance helper functions

--- adamas-app/src/components/PieceOfData.js
+++ adamas-app/src/components/PieceOfData.js
@@ -1,5 +1,26 @@
 import React, { useState, useEffect } from "react";
 
+function toRadian(degrees) {
+  return (degrees * 2 * Math.PI) / 360;
+}
+
+function geoDistance(coordinates1, coordinates2) {
+  // Haversine formula: https://en.wikipedia.org/wiki/Haversine_formula
+  // Stable formula for short distances
+  // Earth is an ellipsoid, so different latitudes have different radiuses
+  // Earth radius changes from 6357 km ... 6378 km
+  const earthRadiusAtPipishegy = 6366.442 * 1000;
+  const lat1 = toRadian(coordinates1.latitude);
+  const lon1 = toRadian(coordinates1.longitude);
+  const lat2 = toRadian(coordinates2.latitude);
+  const lon2 = toRadian(coordinates2.longitude);
+  return (
+    2 *
+    earthRadiusAtPipishegy *
+    Math.asin(Math.sqrt(Math.pow(Math.sin((lat2 - lat1) / 2), 2) + Math.cos(lat2) * Math.cos(lat1) * Math.pow(Math.sin((lon2 - lon1) / 2), 2)))
+  );
+}
+
 function PieceOfData({ pieceOfData }) {
   const [userCoords, setUserCoords] = useState(null);
 
