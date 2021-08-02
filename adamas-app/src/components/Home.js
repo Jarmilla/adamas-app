32d08ec506671cbc37153195c2c0b9b2c9d0db21
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-08-02 20:19:57 +0200

    weatherforecast for 7 day

--- adamas-app/src/components/Home.js
+++ adamas-app/src/components/Home.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Home() {
+  const lat = 47.813778;
+  const lon = 19.976766;
+  const APIkey = "23c16eed06c02fd60c74cdaf0b3558f3";
+  const [weather, setWeather] = useState(null);
+  let today = new Date();
+  const daysOfWeek = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=metric&lang=hu&appid=${APIkey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+        console.log(data);
+      })
+      .catch((err) => console.log("Error:", err));
+  }, []);
+
   return (
     <div>
-      <h2>Home</h2>
-      <p>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000
-        years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from
-        a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from
-        sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on
-        the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus
-        Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-      </p>
+      <h3>Főhadiszállás</h3>
+      <p>Koordináták</p>
+
+      {weather ? (
+        <ul>
+          <li>{weather.current.temp} °C</li>
+          <li>ikonkép</li>
+          <li>Szélsebbesség: {weather.current.wind_speed}</li>
+          <li>{weather.current.weather[0].description}</li>
+        </ul>
+      ) : (
+        "Loading..."
+      )}
+      {weather?.daily.map((day) => (
+        <div key={day.dt}>
+          <div>{daysOfWeek[new Date(day.dt * 1000).getDay()]}</div>
+          <div>{day.temp.day} °C</div>
+          <div>{day.temp.max} °C</div>
+          <div>{day.temp.min} °C</div>
+          <div>{day.wind_speed}</div>
+          <div>{day.rain} mm</div>
+
+          {/* <WeatherIcon code={day.weather[0].icon} size="50px"/> */}
+          <div>{day.weather[0].description}</div>
+        </div>
+      ))}
     </div>
   );
 }
